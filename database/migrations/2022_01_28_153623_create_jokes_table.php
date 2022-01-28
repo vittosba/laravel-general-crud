e1c3d6978e1d9408a3b51080bff175c0d9549b4d
vittosba <vitsbardella@gmail.com>
2022-01-28 16:39:31 +0100

    add migration and model

---
+++ database/migrations/2022_01_28_153623_create_jokes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJokesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jokes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('author', 50);
+            $table->text('body');
+            $table->date('date_of_publication');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jokes');
+    }
+}
